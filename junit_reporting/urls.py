b5743838c159d853da4a4536db01980abdc3f91b
Felix Morgner <felix.morgner@gmail.com>
2017-10-13 22:47:37 +0200

    urls: add missing URLs

--- junit_reporting/urls.py
+++ junit_reporting/urls.py
@@ -1,13 +1,33 @@
 # pylint: disable=missing-docstring
-from os.path import join
 from django.conf.urls import url
-from django.conf.urls.static import static
-from django.conf import settings
 
-from junit_reporting.views import IndexView, ReportUploadView
+from junit_reporting.views import (
+    IndexView,
+    ReportView,
+    ReportUploadView,
+    SuiteView,
+    TestView,
+)
+
+
+REPORT_URL = (
+    r'^report/(?P<build_number>[1-9][0-9]+)/?'
+)
+SUITE_URL = (
+    r'^report/(?P<build_number>[1-9][0-9]+)/'
+    r'(?P<suite_name>[.a-zA-Z]+)/?'
+)
+TEST_URL = (
+    r'^report/(?P<build_number>[1-9][0-9]+)/'
+    r'(?P<suite_name>[.a-zA-Z]+)/'
+    r'(?P<test_name>[.a-zA-Z]+)/?'
+)
 
 # pylint: disable=invalid-name
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^upload/(?P<build_number>[1-9][0-9]+)$', ReportUploadView.as_view()),
+    url(REPORT_URL, ReportView.as_view(), 'report'),
+    url(SUITE_URL, SuiteView.as_view(), 'suite'),
+    url(TEST_URL, TestView.as_view(), 'test'),
 ]
